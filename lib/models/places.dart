f8848c0fdfbccf75783c7eee19830820524c5f66
Swam Yee Zaw <swamsimon13@gmail.com>
2025-05-03 15:53:20 +0700

    using reverse geocoding API and storing location Data in the Model

--- lib/models/places.dart
+++ lib/models/places.dart
@@ -3,12 +3,27 @@ import 'dart:io';
 
 const uuid = Uuid();
 
+class Placelocation {
+  Placelocation({
+    required this.address,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  final double latitude;
+  final double longitude;
+  final String address;
+}
+//saving the location data in the model
+
 class Places {
   final String id;
   final String title;
   final File image;
+  final Placelocation location;
 
-  Places({required this.title, required this.image}) : id = uuid.v4();
+  Places({required this.title, required this.image, required this.location})
+    : id = uuid.v4();
 }
 
 //dynamic id for places 
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:http/http.dart' as http;    
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
 
   @override
-  State<LocationInput> createState() => _LocationInputState();
+  State<LocationInput> createState() => LocationInputState();
 }
 
-class _LocationInputState extends State<LocationInput> {
+class LocationInputState extends State<LocationInput> {
+  var _isGettinglocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    setState(() {
+      _isGettinglocation = true;
+    });
+
+    locationData = await location.getLocation();
+    // final url = Uri.parse("https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY");
+
+    setState(() {
+      _isGettinglocation = false;
+    });
+    print(locationData.longitude!);
+    print(locationData.latitude);
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      "No location chosen",
+      style: Theme.of(context).textTheme.labelLarge!.copyWith(
+        color: Theme.of(context).colorScheme.onSurface,
+      ),
+    );
+
+    if (_isGettinglocation) {
+      previewContent = CircularProgressIndicator();
+    }
+
     return Column(
       children: [
         Container(
@@ -22,18 +73,13 @@ class _LocationInputState extends State<LocationInput> {
               color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
             ),
           ),
-          child: Text(
-            "No location chosen",
-            style: Theme.of(context).textTheme.labelLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onSurface,
-            ),
-          ),
+          child: previewContent,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
               label: Text('Get current Location'),
               icon: Icon(Icons.location_on),
             ),
